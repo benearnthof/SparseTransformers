71738a33e4f2717b55b6518498ed11cd612cab31
benearnthof <benearnthof@hotmail.de>
2025-09-15 16:23:16 +0000

    troubleshoot ZeRO-3

--- setup.sh
+++ setup.sh
@@ -21,13 +21,15 @@ pip install mpi4py
 apt-get update
 apt-get install -y libopenmpi-dev openmpi-bin
 pip install --force-reinstall --no-cache-dir mpi4py
+
+# TODO: move wandb logging to beginning of train file so we log full debug info
+wandb init
 # make sure to export these on new GPU nodes
-export DEEPSPEED_COMM_BACKEND=nccl
-export NCCL_P2P_LEVEL=SYS
-# for multinode? 
+# export DEEPSPEED_COMM_BACKEND=nccl
+# export NCCL_P2P_LEVEL=SYS
+# # for multinode? 
 
 nvidia-smi topo -m
-wandb init
 
-torchrun --standalone --nnodes=1 --nproc_per_node=4 train.py
+torchrun --nproc_per_node=2 train.py
 
